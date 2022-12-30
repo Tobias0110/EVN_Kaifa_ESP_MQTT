d5d78a76d575be6907d46a3ddcf22f35e497ce37
PreyMa <mlpreymann@gmail.com>
2022-12-30 12:13:20 +0100

    Send RSSI and IP in JSON mqtt messages

--- Kaifa_to_MQTT_ESP8266/Kaifa_to_MQTT_ESP8266.ino
+++ Kaifa_to_MQTT_ESP8266/Kaifa_to_MQTT_ESP8266.ino
@@ -988,6 +988,10 @@ public:
             sender.appendField(value);
         }
 
+        void appendField(const char* name, const char* value) {
+            sender.appendField(name, value);
+        }
+
     private:
         MqttSender& sender;
     };
@@ -1000,6 +1004,7 @@ protected:
     virtual void appendField(const CosemTimestamp&) = 0;
     virtual void appendField(const CosemScaledValue&) = 0;
     virtual void appendField(const CosemMeterNumber&) = 0;
+    virtual void appendField(const char* name, const char* value) = 0;
     virtual void endFieldTransmission() = 0;
 };
 
@@ -2072,8 +2077,7 @@ public:
         }
     }
 
-    void mqttPublish(MqttSender& sender) {
-        auto transmission = sender.transmitFields();
+    void mqttPublish(MqttSender::FieldTransmission& transmission) {
         transmission.appendField(meterNumber);
         transmission.appendField(timestamp);
 
@@ -2169,6 +2173,7 @@ protected:
     virtual void appendField(const CosemTimestamp& timestamp) override {}
     virtual void appendField(const CosemScaledValue& value) override {}
     virtual void appendField(const CosemMeterNumber&) override {}
+    virtual void appendField(const char* name, const char* value) override {}
     virtual void endFieldTransmission() override {}
 };
 
@@ -2204,6 +2209,8 @@ protected:
         this->client.publish(this->path, printer.cString(), false);
     }
 
+    virtual void appendField(const char* name, const char* value) override {}
+
     virtual void endFieldTransmission() override {}
 };
 
@@ -2226,15 +2233,19 @@ protected:
     }
 
     virtual void appendField(const CosemMeterNumber& meterNumber) override {
-        beginField("meternumber");
-        printer.printChar('"');
-        printer.print(meterNumber.cString());
-        printer.printChar('"');
+        appendField("meternumber", meterNumber.cString());
     }
 
     virtual void appendField(const CosemScaledValue& value) override {
         beginField(value.fieldLabel().endpoint());
         value.serialize(printer);
+    }
+
+    void appendField(const char* name, const char* value) override {
+        beginField(name);
+        printer.printChar('"');
+        printer.print(value);
+        printer.printChar('"');
     }
 
     void init() {
@@ -2286,6 +2297,22 @@ private:
 #define LOW    0x00
 #define HIGH   0x01
 #define WL_CONNECTED 0x0A
+
+class DummyIPAddress {
+public:
+    DummyIPAddress(u8 a, u8 b, u8 c, u8 d) : octets{ a, b, c, d } {}
+
+    u8 operator[](u8 x) const {
+        return octets[x];
+    }
+private:
+    u8 octets[4];
+};
+
+std::ostream& operator<<(std::ostream& o, const DummyIPAddress addr) {
+    o << addr[0] << '.' << addr[1] << '.' << addr[2] << '.' << addr[3];
+    return o;
+}
 
 class DummySerial {
 public:
@@ -2384,6 +2411,12 @@ public:
     u32 print(u32 val) {
         assert(didBegin);
         std::cout << val;
+        return 1;
+    }
+
+    u32 print(const DummyIPAddress& addr) {
+        assert(didBegin);
+        std::cout << addr;
         return 1;
     }
 
@@ -2500,7 +2533,8 @@ public:
 
     void hostname(const char*) {}
 
-    const char* localIP() const { return "<dummy-ip>"; }
+    DummyIPAddress localIP() const { return {1,2,3,4}; }
+    i8 RSSI() const { return -83; }
 
     void begin(const char* ssid, const char* pwd) {
         std::cout << "[!] WIFI ssid: '" << ssid << "' password: '" << pwd << "'\r\n";
@@ -2788,7 +2822,22 @@ ErrorOr<void> waitForAndProcessPacket() {
 #endif
     TRY(mqttSender->connect());
     mqttSender->publishRaw(serialReader.allDataRead());
-    cosemData.mqttPublish(*mqttSender);
+    {
+        auto transmission = mqttSender->transmitFields();
+        cosemData.mqttPublish(transmission);
+
+        LocalBuffer<20> printBuffer;
+        BufferPrinter printer{ printBuffer };
+
+        // Append additional fields only sent via JSON
+        auto ip = WiFi.localIP();
+        printer.printUnsigned(ip[0]).printChar('.').printUnsigned(ip[1]).printChar('.').printUnsigned(ip[2]).printChar('.').printUnsigned(ip[3]);
+        transmission.appendField("ip", printer.cString());
+
+        printer.clear();
+        printer.print((i16)WiFi.RSSI()).print("dBm");
+        transmission.appendField("rssi", printer.cString());
+    }
 
     return {};
 }
